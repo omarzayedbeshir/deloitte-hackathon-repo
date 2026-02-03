2668fd2fff11a6ec58d68632d324265012b6a168
omarzayedbeshir <omarzayed7777@gmail.com>
2026-02-03 14:44:44 +0200

    Added dotenv variables usage

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = "super-secret-key-change-this"
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "jwt-secret-key-change-this"
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
